53e7f2650db83c4fdb4992337beb64d26cc25962
Jordan Olshevski <jordanolshevski@gmail.com>
2015-11-15 18:38:54 -0800

    Rename constructor from shelltest to constructor
    I'm renaming the shelltest function to constructor for the sake of being
    more explicit. This will make sense in light of future refactoring.

--- shelltest.js
+++ shelltest.js
@@ -3,19 +3,19 @@ var process = require('child_process');
 var assert = require('assert');
 
 module.exports = function() {
-  return new shelltest;
+  return new constructor;
 }
 
-var shelltest = function() {
+var constructor = function() {
   this.options = {};
   this.expectations = [];
 
-  shelltest.prototype.cmd = function(val) {
+  constructor.prototype.cmd = function(val) {
     this.cmd = val;
     return this;
   };
 
-  shelltest.prototype.expect = function(var1, var2) {
+  constructor.prototype.expect = function(var1, var2) {
     if (arguments.length > 1) {
       this.expectations.push({ "type": var2.constructor.name, "matcher": var1, "value": var2 });
     } else {
@@ -25,13 +25,13 @@ var shelltest = function() {
   };
 
   [ "cwd", "env", "timeout", "uid", "gid" ].forEach(function(opt) {
-    shelltest.prototype[opt] = function(val) {
+    constructor.prototype[opt] = function(val) {
       this.options[opt] = val;
       return this;
     };
   });
 
-  shelltest.prototype.end = function(cb) {
+  constructor.prototype.end = function(cb) {
     var expectations = this.expectations;
     if (this.cmd === null) { throw new Error(".end called before command set") }
     process.exec(this.cmd, this.options, function(err, stdout, stderr){
